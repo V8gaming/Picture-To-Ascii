7c8f8138ae682d9fc53e85459af95e3bdc9300cc
Sam Voss <samvoss69@gmail.com>
2020-11-11 19:50:38 +1100

    Added scale option.

--- Picture To Ascii/Picture_To_Ascii.py
+++ Picture To Ascii/Picture_To_Ascii.py
@@ -4,21 +4,27 @@ import math
 import os
 
 ASCII_CHARS = ["@", "#", "$", "%", "?", "*", "+", ";", ":", ",", ".", " "]
-def mainmain():
+def ainput():
     path = input("Input valid path name to image:\n")
     try:
         image = PIL.Image.open(path) 
     except:
         print(path, "Is not a valid path to picture.")
     return(path)
-path = mainmain()
+path = ainput()
+
+scale = input("Scale 1 is normal(in decimal e.g. 0.5, 1.5 etc), leave empty for normal:\n")
+
+
+if scale == "":
+    scale = 1
 
 
 im = Image.open(path)
 width = im.size[0]
-height = im.size[1]
+height = im.size[1] 
 
-im = im.resize((int(width /100 * 165), height), 2)
+im = im.resize((int(float(width /100 * 165) * float(scale)), int(float(height) * float(scale))), 2)
 im = im.save("Temp.png")
 im = Image.open("Temp.png")
 new_width = im.size[0]
